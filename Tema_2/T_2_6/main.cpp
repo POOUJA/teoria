b543accb6dd1d381ffd592bda41341b0f9f1fdfd
Victor M. Rivas Santos <vrivas@ujaen.es>
2015-09-27 20:25:54 +0200

    Añado el ejemplo T_2_6 para enseñar cómo funciona el destructor

---
+++ Tema_2/T_2_6/main.cpp
@@ -0,0 +1,60 @@
+/** 
+ * @brief Ejemplo T_2_6 de teoría: Entrada de Facebook
+ * @file main.cpp
+ * @author Victor M. Rivas Santos <vrivas@ujaen.es>
+ * @date 27 de septiembre de 2015, 11:00
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include "EntradaEnFacebook.h"
+using namespace std;
+
+/**
+ * @brief Función principal
+ * @argc Número de argumentos pasados por línea de órdenes
+ * @argc Caractares pasados como argumentos por línea de órdenes
+ * @return 0 si todo funciona bien; distinto de 0 en otro caso.
+ */
+int main(int argc, char** argv) {
+    // Creamos una entrada automática
+    EntradaEnFacebook entrada("Hoy mi gato se ha dormido encima de la lavadora", 30);
+
+    // Añadimos comentarios
+    entrada.AddComentario("Qué lindo tu gato!");
+    entrada.AddComentario("El mío prefiere dormir encima del sofá");
+    entrada.AddComentario("Ja, ja... espero que no le dé por meterse dentro");
+
+    // Mostramos los datos almacenados
+    cout << "Entrada: " << endl
+            << "    " << entrada.GetTexto() << endl;
+    for (int i = 0; i < entrada.GetNumComentarios(); ++i) {
+        cout << "         - " << entrada.GetComentario(i) << endl;
+    }
+
+
+    // Ahroa creamos una entrada dinámica
+    EntradaEnFacebook *otraEntrada = 0;
+    otraEntrada = new EntradaEnFacebook("No pillo los ejemplos de POO", 30);
+
+    // Añadimos comentarios
+    otraEntrada->AddComentario("Ánimo!! El primer año no es fácil");
+    otraEntrada->AddComentario("¿Te los has descargado de GitHub? Yo lo hice y me funcionó");
+
+    // Mostramos los datos almacenados
+    cout << "Entrada: " << endl
+            << "    " << otraEntrada->GetTexto() << endl;
+    for (int i = 0; i < otraEntrada->GetNumComentarios(); ++i) {
+        cout << "         - " << otraEntrada->GetComentario(i) << endl;
+    }
+
+    // Llamamos explícitamente al destructor del objeto dinámico
+    if( otraEntrada ) {
+        delete otraEntrada;
+        otraEntrada=0;
+    }
+    
+    // No es necesario llamar al destructor del objeto automático.
+    return 0;
+}
+
