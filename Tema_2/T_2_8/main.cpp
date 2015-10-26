d4a36b4bcad95574c1fbaa40870b103a2575ac28
Victor M. Rivas Santos <vrivas@ujaen.es>
2015-10-26 22:48:19 +0100

    Separo el try ... catch del throw en uno de los ejemplos

--- Tema_2/T_2_8/main.cpp
+++ Tema_2/T_2_8/main.cpp
@@ -11,6 +11,18 @@
 using namespace std;
 
 /**
+ * @brief Divide a entre b comprobando que b no sea 0
+ * @param a Dividendo
+ * @param b Divisor
+ * @post Devuelve el resultado de a/b, si b es distinto de 0
+ * @throws MiExcepcion Si be es 0
+ * @return El resultado de dividir a entre b
+ */
+float dividir( float a, float b) {
+    if( b==0 ) throw MiExcepcion( "Función dividir: No se puede dividir por 0");
+    return a/b;
+}
+/**
  * @brief Función principal
  * @argc Número de argumentos pasados por línea de órdenes
  * @argc Caractares pasados como argumentos por línea de órdenes
@@ -22,8 +34,7 @@ int main(int argc, char** argv) {
 
     // Primero creamos una excepción, para luego lanzarla
     cout << "+ En primer lugar creamos un objeto excepción que luego lanzaremos" << endl;
-
-    MiExcepcion fueraDeRango("La posición indicada está fuera del rango válido");
+    MiExcepcion fueraDeRango("Función main: La posición indicada está fuera del rango válido");
 
     std::string profesores[] = {"José Ramón Balsas", "Angel Luis García", "Victor Rivas"};
     try {
@@ -39,18 +50,17 @@ int main(int argc, char** argv) {
     }
     cout << endl;
 
-    cout << "+ En segundo lugar, creamos la excepción en el propio throw" << endl << endl;
+    cout << "+ En segundo lugar, usamos una excepción creada en el propio throw (dentro de la función dividir)" 
+            << endl 
+            << endl;
 
     try {
         double notaDePracticas = 7.3;
         double notaDeTeoria = 5.5;
         int numEjerciciosSinHacer = 0;
-        if (numEjerciciosSinHacer <= 0) {
-            throw MiExcepcion("No se puede dividir por 0");
-        }
-        
         // La siguiente instrucción no llega a ejecutarse, salvo que cambiemos el valor de numEjerciciosSinHacer
-        cout << "    - Nota final: " << (notaDePracticas + notaDeTeoria) / numEjerciciosSinHacer << endl;
+        // Nótese que el throw se hace en el función dividir yq ue la excepción se captura aquí.
+        cout << "    - Nota final: " << dividir (notaDePracticas + notaDeTeoria, numEjerciciosSinHacer )<< endl;
     } catch (MiExcepcion &e) {
         cerr << "Error: " << e.GetTexto() << endl;
     }
@@ -62,7 +72,7 @@ int main(int argc, char** argv) {
     try {
         int *variableNoInicializada = 0;
         if (!variableNoInicializada) {
-            throw new MiExcepcion("No puede accederse al contenido de un puntero a NULL");
+            throw new MiExcepcion("Función main: No puede accederse al contenido de un puntero a NULL");
         }
         
         // Las siguientes instrucciones no se ejecutan, salvo que hagamos un new para la variableNoInicializada
