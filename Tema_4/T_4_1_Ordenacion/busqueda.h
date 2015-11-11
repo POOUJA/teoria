19af37d1a7b93a290623d849b66c7cc559037537
Jose Ramon Balsas <jrbalsas@ujaen.es>
2015-11-11 13:54:38 +0100

    Añadido ejemplo 1 del tema 4 sobre uso de plantillas de función

---
+++ Tema_4/T_4_1_Ordenacion/busqueda.h
@@ -0,0 +1,31 @@
+/** Utilidades para la búsqueda de elementos
+ * @file   busqueda.h
+ * @author jrbalsas
+ *
+ * @date 4 de noviembre de 2015, 13:43
+ */
+
+#ifndef BUSQUEDA_H
+#define	BUSQUEDA_H
+
+
+/**Localiza un elemento en un vector
+   @pre  Los elementos del vector deben poder compararse con el operador ==
+   @post Devuelve la posicion del vector donde se encuentra la primera ocurrencia del elemento
+   @post Devuelve -1 si el elemento no se encuentra en el vector
+ */
+template<typename T>
+int BuscaElemento(T elementos[], int numElementos, const T& elemento) {
+
+    int posEncontrado=-1;
+    
+    for (int i=0; i<numElementos && posEncontrado==-1; i++) {
+        if (elementos[i]==elemento) 
+            posEncontrado=i;
+    }
+    return posEncontrado;
+    
+}
+
+#endif	/* ORDENACION_H */
+
---
+++ Tema_4/T_4_1_Ordenacion/main.cpp
@@ -0,0 +1,54 @@
+/** Ejemplo de búsqueda y ordenación de elementos
+ * @file   main.cpp
+ * @author jrbalsas@ujaen.es
+ *
+ * @date 4 de noviembre de 2015, 14:17
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "busqueda.h"
+#include "ordenacion.h"
+
+using namespace std;
+
+/**Visualiza un vector de enteros*/
+void Visualiza(int elementos[], int numElementos) {
+    std::cout << "[";
+    for (int i = 0; i < numElementos; i++) {
+        std::cout   << (i>0?", ":"")
+                    << elementos[i];
+    }
+    std::cout << "]" << std::endl;
+}
+
+
+/**Ejemplo de búsqueda y ordenación de elementos en vectores 
+ */
+const int NUMELEMENTOS=10;
+int main(int argc, char** argv) {
+
+    int elementos[]={10, 5 ,8 , 2, 7, 25, 1, 12, 16, 5};
+    
+    std::cout << "Operaciones con enteros" << std::endl << std::endl;
+    std::cout << "Vector inicial" << std::endl;
+    Visualiza(elementos,NUMELEMENTOS);
+    
+    int valor=1;
+    std::cout << "Buscar valor " << valor << std::endl;
+    int posicion=BuscaElemento(elementos,NUMELEMENTOS,valor);
+    std::cout << "El elemento " << valor;
+    if (posicion>=0) {
+        std::cout << " está en la posición " << posicion;
+    } else {
+        std::cout << " no está en el vector";
+    }
+    std::cout << std::endl;
+    
+    std::cout << "Ordenamos el vector" << std::endl;
+    Ordena(elementos,NUMELEMENTOS);
+    Visualiza(elementos,NUMELEMENTOS);
+       
+    return 0;
+}
+
