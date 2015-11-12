77ea6404ae9d487587661f0c8ebd7cddfc901981
Jose Ramon Balsas <jrbalsas@ujaen.es>
2015-11-12 10:40:42 +0100

    Añadida plantilla de funcion para volcado de vectores de elementos de tipo ItemCSV a disco
    y correccion de algunas erratas en comentarios

--- Tema_4/T_4_4_Clases_Abstractas/main.cpp
+++ Tema_4/T_4_4_Clases_Abstractas/main.cpp
@@ -7,23 +7,21 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include "Libro.h"
 #include "Revista.h"
 #include "busqueda.h"
 
+const int NUMEJEMPLARES=4; /*< Tamaño del vector de ejemplares*/
+const std::string FICHEROEJEMPLARES="ejemplares.csv"; /*< Fichero donde se almacenan/recuperan los ejemplares*/
 
 /**Visualiza elementos que implementan la Interfaz CSV*/
 void Visualiza(const ItemCSV &item ) {
     //Polimorfismo de objetos mediante referencias en paso de parámetro
+    //El método sólo conoce el tipo de objetos por el comportamiento que ofrecen de la interfaz
     //Polimorfismo de método en llamada a método según el tipo de ejemplar (enlace dinámico)
     std::cout << item.toCSV();
 }
-
-/**Guarda en un fichero elementos que implementan la Interfaz CSV*/
-//void Guarda(ItemCSV* elementos[], int numElementos, std::string nombreFichero ) {
-// NO ES VIABLE CONVERSION DE Ejemplar** a ItemCSV** sin plantillas
-//}
-
 
 /**Visualiza los ejemplares de un vector*/
 void Visualiza(Ejemplar* elementos[], int numElementos ) {
@@ -39,7 +37,30 @@ void Visualiza(Ejemplar* elementos[], int numElementos ) {
     std::cout << std::endl;
 }
 
-const int NUMEJEMPLARES=4; /*< Tamaño del vector de ejemplares*/
+/**Guarda en un fichero elementos que implementan la Interfaz CSV
+   @param[in] elementos vector de punteros a objetos de la clase T
+   @param[in] nombreFichero ruta del fichero donde se almacenarán los elementos
+   @pre la clase T implementa la interfaz ItemCSV
+   @return devuelve un valór lógico indicando si la operación ha tenido éxito*/
+template<class T>
+bool GuardaCSV(T* elementos[], int numElementos, std::string nombreFichero ) {
+    std::ofstream f;
+    std::string linea;
+    bool guardado=false;
+
+    f.open(nombreFichero.c_str());
+    
+    if (f.good()) {
+        for ( int i=0; i<numElementos; i++ ) {
+            //Polimorfismo de métodos usando la interfaz ItemCSV
+            linea=linea+elementos[i]->toCSV();
+            f << linea << std::endl;
+        };
+        f.close();
+        guardado=true;
+    }
+    return guardado;
+}
 
 /**
  * Ejemplo de prueba clases Ejemplar y Libro
@@ -61,7 +82,7 @@ int main(int argc, char** argv) {
     Ejemplar *previstaMUY=new Revista(revistaMUY);
     Ejemplar *plibroQuijote=new Libro(libroQuijote);
     
-    //Utilizamos polimorfirmos de objetos mediante punteros a clase base
+    //Utilizamos polimorfismo de objetos mediante punteros a clase base
     Ejemplar* ejemplares[NUMEJEMPLARES]= {
         previstaMUY,
         plibroQuijote,
@@ -71,6 +92,15 @@ int main(int argc, char** argv) {
     
     //Visualizamos todos los ejemplares
     Visualiza(ejemplares,NUMEJEMPLARES);
+    
+    //Almacenamos los ejemplares en un fichero
+    if (GuardaCSV(ejemplares,NUMEJEMPLARES,FICHEROEJEMPLARES)) {
+        std::cout << "Ejemplares almacenados en fichero "
+                  << FICHEROEJEMPLARES << std::endl << std::endl;
+    } else {
+        std::cerr << "Ha ocurrido un error al guardar los ejemplares en fichero "
+                  << FICHEROEJEMPLARES << std::endl;
+    }
 
     //Búsqueda de un ejemplar en el vector de ejemplar
     Libro ejemAux("9788467016901"); //Vamos a buscar el Quijote
