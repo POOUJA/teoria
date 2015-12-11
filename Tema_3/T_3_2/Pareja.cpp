107ead4c870425c4381c645297b4a8d0a5ab665b
algarcia <algarcia@algarcia-All-Series>
2015-12-11 12:14:42 +0100

    Añadido archvo Doxyfile, y comentarios

--- Tema_3/T_3_2/Pareja.cpp
+++ Tema_3/T_3_2/Pareja.cpp
@@ -1,23 +1,39 @@
-/* 
- * File:   Pareja.cpp
- * Author: algarcia
- * 
- * Created on 9 de diciembre de 2015, 12:12
+/**
+ * @file Pareja.h
+ * Archivo con la implementación de la clase Pareja
+ * @author algarcia
+ * @date 2015-12-09
  */
 
 #include <sstream>
 #include <stdexcept>
 #include "Pareja.h"
 
+/**
+ * Inicializa los punteros a NULL, y el ranking a 99999
+ * @brief Constructor por defecto
+ */
 Pareja::Pareja ( ): t1 (NULL), t2 (NULL), ranking (99999)
 {
 }
 
+/**
+ * @brief Constructor de copia
+ * @param orig Objeto del que se copian los atributos
+ */
 Pareja::Pareja ( const Pareja& orig ): t1 (orig.t1), t2 (orig.t2),
                                        ranking (orig.ranking)
 {
 }
 
+/**
+ * @brief Constructor parametrizado
+ * @param nT1 Puntero al primer tenista de la pareja
+ * @param nT2 Puntero al segundo tenista de la pareja
+ * @param nRanking Posición en el ranking de la pareja. Debe ser un número
+ *                 positivo
+ * @throw std::out_of_range Si el valor de ranking no es positivo
+ */
 Pareja::Pareja ( Tenista *nT1, Tenista *nT2, int nRanking ): t1 (nT1), t2 (nT2)
 {
    if ( nRanking > 0 )
@@ -30,6 +46,14 @@ Pareja::Pareja ( Tenista *nT1, Tenista *nT2, int nRanking ): t1 (nT1), t2 (nT2)
    }
 }
 
+/**
+ * @brief Constructor parametrizado
+ * @param nT1 Referencia al primer tenista de la pareja
+ * @param nT2 Referencia al segundo tenista de la pareja
+ * @param nRanking Posición en el ranking de la pareja. Debe ser un número
+ *                 positivo
+ * @throw std::out_of_range Si el valor de ranking no es positivo
+ */
 Pareja::Pareja ( Tenista &nT1, Tenista &nT2, int nRanking ): t1 (&nT1), t2 (&nT2)
 {
    if ( nRanking > 0 )
@@ -42,12 +66,22 @@ Pareja::Pareja ( Tenista &nT1, Tenista &nT2, int nRanking ): t1 (&nT1), t2 (&nT2
    }
 }
 
+/**
+ * Pone a NULL los punteros a los miembros de la pareja, pero no destruye los
+ * objetos
+ * @brief Destructor
+ */
 Pareja::~Pareja ( )
 {
    t1 = NULL;
    t2 = NULL;
 }
 
+/**
+ * @brief Modificador (setter) del atributo Pareja::ranking
+ * @param nRanking Nuevo valor de ranking. Debe ser un número positivo
+ * @throws std::out_of_range Si el valor de nRanking no es positivo
+ */
 void Pareja::setRanking ( int nRanking )
 {
    if ( nRanking > 0 )
@@ -60,11 +94,21 @@ void Pareja::setRanking ( int nRanking )
    }
 }
 
+/**
+ * @brief Observador (getter) del atributo Pareja::ranking
+ * @return El valor de ranking de la pareja
+ */
 int Pareja::getRanking ( ) const
 {
    return ranking;
 }
 
+/**
+ * Si se pasa NULL como parámetro, la pareja pasa a tener ranking 99999, ya que
+ * no está completa
+ * @brief Método para cambiar el segundo tenista de la pareja
+ * @param nT2 Puntero al nuevo tenista
+ */
 void Pareja::setT2 ( Tenista* nT2 )
 {
    this->t2 = nT2;
@@ -75,11 +119,22 @@ void Pareja::setT2 ( Tenista* nT2 )
    }
 }
 
+/**
+ * @brief Observador (getter) para el atributo Pareja::t2
+ * @return Un puntero al segundo tenista de la pareja, o NULL si éste no ha
+ *         sido asignado
+ */
 Tenista* Pareja::getT2 ( ) const
 {
    return t2;
 }
 
+/**
+ * Si se pasa NULL como parámetro, la pareja pasa a tener ranking 99999, ya que
+ * no está completa
+ * @brief Método para cambiar el primer tenista de la pareja
+ * @param nT1 Puntero al nuevo tenista
+ */
 void Pareja::setT1 ( Tenista* nT1 )
 {
    this->t1 = nT1;
@@ -90,11 +145,21 @@ void Pareja::setT1 ( Tenista* nT1 )
    }
 }
 
+/**
+ * @brief Observador (getter) para el atributo Pareja::t1
+ * @return Un puntero al primer tenista de la pareja, o NULL si éste no ha
+ *         sido asignado
+ */
 Tenista* Pareja::getT1 ( ) const
 {
    return t1;
 }
 
+/**
+ * @brief Método para generar una cadena "user-friendly" de información
+ * @return Una cadena de texto incluyendo los nombres de los tenistas que forman
+ *         la pareja, y la posición en el ranking de la misma
+ */
 string Pareja::info () const
 {
    std::stringstream aux;
@@ -135,6 +200,14 @@ string Pareja::info () const
    return ( resultado );
 }
 
+/**
+ * Sólo copia el valor de ranking, puesto que no tiene sentido tener dos parejas
+ * con los mismos jugadores
+ * @brief Operador de asignación
+ * @param orig Objeto del que se copian los valores de los atributos
+ * @return Una referencia al propio objeto, para su uso en asignaciones en
+ *         cascada (a=b=c)
+ */
 Pareja& Pareja::operator = (const Pareja& orig)
 {
    this->ranking = orig.ranking;
--- Tema_3/T_3_2/Tenista.cpp
+++ Tema_3/T_3_2/Tenista.cpp
@@ -1,23 +1,39 @@
-/* 
- * File:   Tenista.cpp
- * Author: algarcia
- * 
- * Created on 9 de diciembre de 2015, 12:12
+/**
+ * @file Tenista.cpp
+ * Archivo con la implementación de la clase Tenista
+ * @author algarcia
+ * @date 2015-12-09
  */
 
 #include "Tenista.h"
 #include <sstream>     // Para usar stringstream
 #include <stdexcept>   // Para usar std::out_of_range
 
+/**
+ * Inicializa el nombre a "---", y el ranking a 99999
+ * @brief Constructor por defecto de la clase
+ */
 Tenista::Tenista ( ): nombre ("---"), ranking (99999)
 {
 }
 
+/**
+ * Copia el valor de ranking, pero modifica el nombre, añadiendo "-2" al final,
+ * para evitar que haya dos tenistas con el mismo nombre
+ * @brief Constructor de copia
+ * @param orig Objeto del que se copian los atributos
+ */
 Tenista::Tenista ( const Tenista& orig ): ranking ( orig.ranking )
 {
    nombre = orig.nombre + " - 2";
 }
 
+/**
+ * @brief Constructor parametrizado
+ * @param nNombre Nombre del nuevo tenista
+ * @param nRanking Ranking del nuevo tenista. Ha de ser un número positivo
+ * @throw std::out_of_range Si el valor de ranking no es positivo
+ */
 Tenista::Tenista ( const string nNombre, const int nRanking ): nombre (nNombre)
 {
    if ( nRanking > 0 )
@@ -30,10 +46,18 @@ Tenista::Tenista ( const string nNombre, const int nRanking ): nombre (nNombre)
    }
 }
 
+/**
+ * @brief Destructor
+ */
 Tenista::~Tenista ( )
 {
 }
 
+/**
+ * @brief Modificador (setter) del atributo Tenista::ranking
+ * @param nRanking Nuevo valor de ranking. Debe ser un número positivo
+ * @throws std::out_of_range Si el valor de nRanking no es positivo
+ */
 void Tenista::setRanking ( int nRanking )
 {
    if ( nRanking > 0 )
@@ -46,21 +70,38 @@ void Tenista::setRanking ( int nRanking )
    }
 }
 
+/**
+ * @brief Observador (getter) del atributo Tenista::ranking
+ * @return El valor de ranking del tenista
+ */
 int Tenista::getRanking ( ) const
 {
    return ranking;
 }
 
+/**
+ * @brief Modificador (setter) del atributo Tenista::nombre
+ * @param nNombre Nuevo valor para el nombre del tenista
+ */
 void Tenista::setNombre ( string nNombre )
 {
    this->nombre = nNombre;
 }
 
+/**
+ * @brief Observador (getter) para el atributo Tenista::nombre
+ * @return El nombre del tenista
+ */
 string Tenista::getNombre ( ) const
 {
    return nombre;
 }
 
+/**
+ * @brief Método para generar una cadena "user-friendly" de información
+ * @return Una cadena de texto incluyendo los valores de los atributos del
+ *         objeto 
+ */
 string Tenista::info () const
 {
    std::stringstream aux;
@@ -75,6 +116,14 @@ string Tenista::info () const
    return ( resultado );
 }
 
+/**
+ * No se copia el atributo Tenista::nombre, para evitar que haya dos tenistas
+ * con el mismo nombre
+ * @brief Operador de asignación
+ * @param orig Objeto del que se copian los atributos
+ * @return Una referencia al propio objeto, para posibilitar las asignaciones en
+ *         cascada (a=b=c)
+ */
 Tenista& Tenista::operator = (const Tenista& orig)
 {
    this->ranking = orig.ranking;
