d15c92b776679b261c5e2a6f21f4dbfbf141fe3d
algarcia <algarcia@algarcia-SATELLITE-P850>
2016-01-08 19:00:37 +0100

    Nuevo proyecto T_3_6

---
+++ Tema_3/T_3_6/main.cpp
@@ -0,0 +1,20 @@
+/** 
+ * @file %<%NAME%>%.%<%EXTENSION%>%
+ * @author %<%USER%>%
+ *
+ * @date %<%DATE%>%
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * 
+ */
+int main ( int argc, char** argv )
+{
+
+   return 0;
+}
+
